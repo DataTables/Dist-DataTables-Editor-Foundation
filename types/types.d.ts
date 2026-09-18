ff164a3031622c971bd7428022f47d2bea619b1b
Allan Jardine <allan@datatables.net>
2026-09-18 15:40:29 +0000

    Dist: Fix types for the host Editor

--- types/types.d.ts
+++
@@ -1,7 +0,0 @@
-
-// Dist-DataTables-Editor-Foundation integration with Foundation exports the main Editor class having
-// set default values to complete the ingeration.
-import Editor from "datatables.net-editor";
-
-export default Editor;
-
